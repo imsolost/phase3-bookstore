8bcdcb29b34d818a37548496b6b4a84095bf1d8b
Ryan Kent <imsolost@gmail.com>
2017-08-15 15:04:46 -0700

    can now use static css

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
-const app = require('express')()
+const express = require('express')
 const bodyParser = require('body-parser')
 const routes = require('./server/routes')
+const app = express()
 
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
 
-// app.use(exprres.static('public'))
+app.use(express.static('public'))
 app.use(bodyParser.urlencoded({extended: false}))
 
 app.use('/', routes)
