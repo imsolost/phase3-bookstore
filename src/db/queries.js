d4f65a99f52111f00f7e5bb579ea08da1417f9bc
Ryan Kent <imsolost@gmail.com>
2017-08-15 09:42:16 -0700

    got delete button working on home page

--- src/db/queries.js
+++ src/db/queries.js
@@ -1,7 +1,22 @@
 const db = require('./db')
 
+const getBooks = () => {
+  return db.query(`SELECT * FROM books`, [])
+    .catch(error => error)
+}
 
+const deleteBook = (id) => {
+  return db.query(`DELETE FROM books WHERE id = $1`, [id])
+    .catch(error => error)
+}
+
+const search = (input) => {
+  return db.query(`SELECT  * FROM books WHERE lower(title || ' ' || author) LIKE $1`, [`%${input.toLowerCase().replace(/\s+/,'%')}%`])
+    .catch(error => error)
+}
 
 module.exports = {
-
+  getBooks,
+  deleteBook,
+  search
 }
--- src/server/routes.js
+++ src/server/routes.js
@@ -1,7 +1,28 @@
 const router = require('express').Router()
+const booksDb = require('../db/queries')
 
 router.get('/', (req, res) => {
-  res.render('home')
+  booksDb.getBooks()
+    .then( books => res.render('home', { books }) )
+    .catch( err => console.log('err', err) )
+})
+
+router.get('/books/delete/:id', (req, res) => {
+  const id = req.params.id
+  booksDb.deleteBook(id)
+    .then( book => {
+      if (book) return res.redirect('/')
+    })
+    .catch( err => console.log('err', err) )
+})
+
+router.get('/books/search', (req, res) => {
+  const query = req.query.q
+  booksDb.search(query)
+    .then( (books) => {
+      if (books) return res.render('index', { query, books })
+    })
+    .catch( err => console.log('err', err) )
 })
 
 
